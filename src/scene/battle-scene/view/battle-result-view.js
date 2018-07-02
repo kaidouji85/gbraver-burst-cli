2f64c828d32fbcea25c554cfd43a82e585a08966
竹内佑介 <kaidouji85@gmail.com>
2018-07-02 21:31:28 +0900

    戦闘結果ビューを別ファイルに切り出した

---
+++ src/scene/battle-scene/view/battle-result-view.js
@@ -0,0 +1,60 @@
+// @flow
+
+import {inspect} from 'util';
+import type {
+  BattleResult,
+  CriticalHit,
+  Feint,
+  Guard,
+  Miss,
+  NormalHit
+} from "gbraver-burst-core/lib/effect/battle/result/battle-result";
+
+/**
+ * 戦闘結果をコンソール用に整形する
+ *
+ * @param result 戦闘結果
+ * @return 整形結果
+ */
+export function battleResultView(result: BattleResult): string {
+  switch (result.name) {
+    case 'NormalHit':
+      return normalHit(result);
+    case 'Guard':
+      return guard(result);
+    case 'CriticalHit':
+      return criticalHit(result);
+    case 'Miss':
+      return miss(result);
+    case 'Feint':
+      return feint(result);
+    default:
+      return inspect(result, {depth: null});
+
+  }
+}
+
+/** 通常ヒット */
+export function normalHit(result: NormalHit): string {
+  return `ヒット: ${result.damage}`;
+}
+
+/** ガード */
+export function guard(result: Guard): string {
+  return `ガード: ${result.damage}`;
+}
+
+/** クリティカルヒット */
+export function criticalHit(result: CriticalHit): string {
+  return `クリティカル: ${result.damage}`;
+}
+
+/** ミス */
+export function miss(result: Miss): string {
+  return 'ミス';
+}
+
+/** フェイント */
+export function feint(result: Feint): string {
+  return 'フェイント';
+}
--- src/scene/battle-scene/view/battle-view.js
+++ src/scene/battle-scene/view/battle-view.js
@@ -1,14 +1,7 @@
 // @flow
 
 import type {Battle} from "gbraver-burst-core/lib/effect/battle/effect/index";
-import type {
-  BattleResult,
-  CriticalHit,
-  Miss,
-  Guard,
-  NormalHit, Feint
-} from "gbraver-burst-core/lib/effect/battle/result/battle-result";
-import {inspect} from 'util';
+import {battleResultView} from "./battle-result-view";
 
 /**
  * 戦闘結果をコマンドライン用に整形する
@@ -19,42 +12,4 @@ import {inspect} from 'util';
 export function battleView(effect: Battle): string {
   return `攻撃(${effect.attackerBattery}) -> 防御(${effect.defenderBattery})
 ${battleResultView(effect.result)}`;
-}
-
-export function battleResultView(result: BattleResult): string {
-  switch (result.name) {
-    case 'NormalHit':
-      return normalHit(result);
-    case 'Guard':
-      return guard(result);
-    case 'CriticalHit':
-      return criticalHit(result);
-    case 'Miss':
-      return miss(result);
-    case 'Feint':
-      return feint(result);
-    default:
-      return inspect(result, {depth: null});
-
-  }
-}
-
-export function normalHit(result: NormalHit): string {
-  return `ヒット(${result.damage})`;
-}
-
-export function guard(result: Guard): string {
-  return `ガード(${result.damage})`;
-}
-
-export function criticalHit(result: CriticalHit): string {
-  return `クリティカル(${result.damage})`;
-}
-
-export function miss(result: Miss): string {
-  return 'ミス';
-}
-
-export function feint(result: Feint): string {
-  return 'フェイント';
 }
