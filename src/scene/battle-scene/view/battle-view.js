01120bb3cf821899f17861d8a25254b7ca9da48c
竹内佑介 <kaidouji85@gmail.com>
2018-07-02 12:59:02 +0900

    戦闘結果にダメージ表示を追加した

--- src/scene/battle-scene/view/battle-view.js
+++ src/scene/battle-scene/view/battle-view.js
@@ -1,6 +1,14 @@
 // @flow
 
 import type {Battle} from "gbraver-burst-core/lib/effect/battle/effect/index";
+import type {
+  BattleResult,
+  CriticalHit,
+  Miss,
+  Guard,
+  NormalHit, Feint
+} from "gbraver-burst-core/lib/effect/battle/result/battle-result";
+import {inspect} from 'util';
 
 /**
  * 戦闘結果をコマンドライン用に整形する
@@ -9,5 +17,44 @@ import type {Battle} from "gbraver-burst-core/lib/effect/battle/effect/index";
  * @return 整形結果
  */
 export function battleView(effect: Battle): string {
-  return `攻撃(${effect.attackerBattery}) -> 防御(${effect.defenderBattery})`;
+  return `攻撃(${effect.attackerBattery}) -> 防御(${effect.defenderBattery})
+${battleResultView(effect.result)}`;
+}
+
+export function battleResultView(result: BattleResult): string {
+  switch (result.name) {
+    case 'NormalHit':
+      return normalHit(result);
+    case 'Guard':
+      return guard(result);
+    case 'CriticalHit':
+      return criticalHit(result);
+    case 'Miss':
+      return miss(result);
+    case 'Feint':
+      return feint(result);
+    default:
+      return inspect(result, {depth: null});
+
+  }
+}
+
+export function normalHit(result: NormalHit): string {
+  return `ヒット(${result.damage})`;
+}
+
+export function guard(result: Guard): string {
+  return `ガード(${result.damage})`;
+}
+
+export function criticalHit(result: CriticalHit): string {
+  return `クリティカル(${result.damage})`;
+}
+
+export function miss(result: Miss): string {
+  return 'ミス';
+}
+
+export function feint(result: Feint): string {
+  return 'フェイント';
 }
