fa892ef11f5796b887b6ca1e72e51ce2ce116798
竹内佑介 <kaidouji85@gmail.com>
2018-07-02 22:29:15 +0900

    選択したアームドーザを表示する様にした

--- src/scene/select-character/index.js
+++ src/scene/select-character/index.js
@@ -4,6 +4,7 @@ import type {Armdozer} from "gbraver-burst-core/lib/armdozer/armdozer";
 import type {Player, PlayerId} from "gbraver-burst-core/lib/player/player";
 import {selectArmdozer} from "../../input/select-armdozer";
 import {START_MESSAGE} from "./message/start-message";
+import {playerListMessage} from "./message/player-list-message";
 
 /**
  * キャラクター選択シーンを再生する
@@ -14,8 +15,10 @@ import {START_MESSAGE} from "./message/start-message";
  */
 export function selectCharacterScene(playerIdList: PlayerId[], armdozerList: Armdozer[]): Player[] {
   console.log(START_MESSAGE);
-  return playerIdList.map(playerId => {
+  const playerList = playerIdList.map(playerId => {
     const armdozer = selectArmdozer(armdozerList, `${playerId}のアームドーザを選択してください`);
     return {playerId, armdozer};
   });
+  console.log(playerListMessage(playerList));
+  return playerList;
 }
