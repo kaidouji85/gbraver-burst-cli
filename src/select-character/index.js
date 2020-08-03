976f5dcca18dec1262eaa232b127c94f5bff6382
竹内佑介 <kaidouji85@gmail.com>
2020-08-03 13:42:45 +0900

    パイロットに対応した

--- src/select-character/index.js
+++ src/select-character/index.js
@@ -2,18 +2,22 @@
 
 import type {Armdozer, Player, PlayerId} from "gbraver-burst-core";
 import {selectArmdozer} from "./select-armdozer";
+import {selectPilot} from "./select-pilot";
+import type {Pilot} from "gbraver-burst-core/lib/player/pilot";
 
 /**
  * キャラクター選択シーンを再生する
  *
  * @param playerIdList プレイヤーIDリスト
- * @param armdozerList アームドーザリスト
+ * @param armdozers アームドーザマスタ
+ * @param pilots パイロットマスタ
  * @return プレイヤー状態
  */
-export function selectCharacterScene(playerIdList: PlayerId[], armdozerList: Armdozer[]): Player[] {
+export function selectCharacterScene(playerIdList: PlayerId[], armdozers: Armdozer[], pilots: Pilot[]): Player[] {
   const playerList = playerIdList.map(playerId => {
-    const armdozer = selectArmdozer(armdozerList, `$select {playerId} armdozer`);
-    return {playerId, armdozer};
+    const armdozer = selectArmdozer(armdozers, `select ${playerId} armdozer`);
+    const pilot = selectPilot(pilots, `select ${playerId} pilot`)
+    return {playerId, pilot, armdozer};
   });
   return playerList;
 }
